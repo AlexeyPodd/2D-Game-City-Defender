8994306f4e933ed0d147e53e61550e16d1da9578
Alex <qwq199313@gmail.com>
2022-08-01 21:45:25 +0300

    little correction

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from sqripts import *
 
-my_game = Game()
 
-my_game.instruct_player()
-while my_game.level_number < 11 and len(my_game.city):
+def main():
+    my_game = Game()
+    my_game.instruct_player()
+    while my_game.level_number < 11 and len(my_game.city):
+
+        my_game.show_transit_screen()
+        my_game.run_level()
 
     my_game.show_transit_screen()
-    my_game.run_level()
 
-my_game.show_transit_screen()
+
+if __name__ == '__main__':
+    main()
